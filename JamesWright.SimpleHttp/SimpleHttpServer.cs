0a39859c3bceef597a1d7415aae12794b33bc1b1
Ondřej Kotas <ondrej.kotas@solarwinds.com>
2018-08-09 23:29:32 +0200

    Rename App to SimpleHttpServer

---
+++ JamesWright.SimpleHttp/SimpleHttpServer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace JamesWright.SimpleHttp
+{
+    public class SimpleHttpServer
+    {
+        private Server server;
+
+        public SimpleHttpServer()
+        {
+            this.server = new Server(new Listener(), new RouteRepository());
+        }
+
+        public void Start (string gateway = "localhost", int port = 8005)
+        {
+            AutoResetEvent keepAlive = new AutoResetEvent(false);
+            this.server.StartAsync(gateway, Convert.ToString(port));
+            keepAlive.WaitOne();
+        }
+
+        public void Get(string endpoint, Action<Request, Response> handler)
+        {
+            this.server.RouteRepository.Get.Add(endpoint, handler);
+        }
+
+        public void Post(string endpoint, Action<Request, Response> handler)
+        {
+            this.server.RouteRepository.Post.Add(endpoint, handler);
+        }
+
+        public void Put(string endpoint, Action<Request, Response> handler)
+        {
+            this.server.RouteRepository.Put.Add(endpoint, handler);
+        }
+
+        public void Delete(string endpoint, Action<Request, Response> handler)
+        {
+            this.server.RouteRepository.Delete.Add(endpoint, handler);
+        }
+    }
+}
